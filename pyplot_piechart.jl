f83baf884119610fbbdd86406bcb9986212ba8ed
gizmaa <gizmaa@gmail.com>
2016-03-08 16:22:32 +0100

    Added font tweaks to the pie chart example. The Gist and iJulia examples still need to be updated.

--- pyplot_piechart.jl
+++ pyplot_piechart.jl
@@ -3,9 +3,9 @@
 #	Demonstrate plotting of pie charts
 #
 # gizmaa (https://gist.github.com/gizmaa/7214002)
-# Julia 0.4.1
+# Julia 0.4.2
 # Created: 29.10.13
-# Last Edit: 26.11.15
+# Last Edit: 08.03.16
 
 using PyPlot
 
@@ -18,6 +18,7 @@ sizes = NaN*zeros(4)
 explode = zeros(length(sizes))
 explode[2] = 0.1 # Move slice 2 out by 0.1
 sizes = [15; 30; 45; 10]
+font = Dict("fontname"=>"Sans","weight"=>"semibold")
 
 ###############
 #  Pie Chart  #
@@ -29,7 +30,8 @@ p = pie(sizes,
 		startangle=90,
 		explode=explode,
 		colors=colors,
-		autopct="%1.1f%%")
+		autopct="%1.1f%%",
+		textprops=font)
 
 axis("equal")
 title("Beer")
