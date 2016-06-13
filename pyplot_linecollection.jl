3f7716d6b32afd4ff6de11909a1ab3eb5db1090c
gizmaa <gizmaa@gmail.com>
2016-06-13 14:07:02 +0200

    Line Collection example added

---
+++ pyplot_linecollection.jl
@@ -0,0 +1,53 @@
+# linecollection.jl
+#
+#   Line Collection plot example
+#
+# Julia 0.4.5
+# 10.06.2016
+# Last Edit: 13.06.2016
+
+#include("/home/jeremy/AD-Home/Code/Julia/Jeremy/linecollection_example.jl")
+
+# Reference: David P. Sanders - https://groups.google.com/d/msg/julia-users/BGcEOZVw8YU/Oacy1O5zBQAJ
+
+using PyPlot
+
+####################
+##  Prepare Data  ##
+####################
+# lines = Any array of tuples of floating point pairs
+
+# First Segment Group
+xs = [1.,3.,5.,0.]
+ys = [2.,4.,.06,0.]
+lines = Any[collect(zip(xs,ys))]
+# lines = [(1.0,2.0),(3.0,4.0),(5.0,0.06),(0.0,0.0)]
+
+# Second Segment Group
+xs = [3.,4]
+ys = [5.,6]
+push!(lines,collect(zip(xs,ys)))
+
+# Third Segment Group
+xs = [8.,2]
+ys = [2.,4]
+push!(lines,collect(zip(xs,ys)))
+
+##############
+##  Colors  ##
+##############
+c = Vector{Int}[[1,0,0],[0,1,0],[0,0,1]]
+
+# Assemble everything into a LineCollection
+line_segments = matplotlib[:collections][:LineCollection](lines,colors=c)
+
+############
+##  Plot  ##
+############
+fig = figure("Line Collection Example",figsize=(10,10))
+ax = axes()
+xlabel("Something")
+ylabel("Something Else")
+ax[:add_collection](line_segments)
+axis("image")
+axis("tight")
